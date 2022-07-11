025cb1eb7690e723b91055154d92cf71cf0084e0
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2022-07-12 01:20:16 +0800

    Update sync_tekton_images.sh

--- tekton/sync_tekton_images.sh
+++ tekton/sync_tekton_images.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+set -x
+S_REGISTRY="gcr.io/tekton-releases/github.com/tektoncd/pipeline/cmd/"
+T_REGISTRY="hbstarjason"
+
 wget https://github.com/sigstore/rekor/releases/download/v0.9.1/rekor-cli-linux-amd64 && \
 chmod +x rekor-cli-linux-amd64 && \
 cp rekor-cli-linux-amd64 /usr/local/bin/rekor-cli && \
@@ -19,7 +23,22 @@ REKOR_ATTESTATION_IMAGES=$(rekor-cli get --uuid "$REKOR_UUID" --format json | jq
 curl "$RELEASE_FILE" > release.yaml
 
 # For each image in the attestation, match it to the release file
+
+## printf $image; grep -q $image release.yaml && echo " ";
 for image in $REKOR_ATTESTATION_IMAGES; do
-  printf $image; grep -q $image release.yaml && echo " ";
+  {
+  cat  >>  tekton_images_list <<EOF
+  $image
+EOF  
+  }
 done
 
+cat tekton_images_list  | while read line
+do 
+    echo ${line}
+       
+    docker pull ${line}
+    #docker tag ${line} 
+    #docker images |grep ${S_REGISTRY}
+    #docker images |grep ${T_REGISTRY}
+done 
